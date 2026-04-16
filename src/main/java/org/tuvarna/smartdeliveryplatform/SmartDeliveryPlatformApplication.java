d4cea566a31ec62fe09fc3f90296c1c1b121e185
Petyo156 <petyo156bg@gmail.com>
2026-04-16 23:00:43 +0300

    Initial commit

---
+++ src/main/java/org/tuvarna/smartdeliveryplatform/SmartDeliveryPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.tuvarna.smartdeliveryplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeliveryPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDeliveryPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tuvarna/smartdeliveryplatform/SmartDeliveryPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tuvarna.smartdeliveryplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeliveryPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
