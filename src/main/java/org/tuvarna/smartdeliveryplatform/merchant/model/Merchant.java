f07596d9ce66357e0bd2d52a8694e61e221018b6
Petyo156 <petyo156bg@gmail.com>
2026-05-06 14:20:55 +0300

    added isClosed field in merchant entity

--- src/main/java/org/tuvarna/smartdeliveryplatform/merchant/model/Merchant.java
+++ src/main/java/org/tuvarna/smartdeliveryplatform/merchant/model/Merchant.java
@@ -42,6 +42,9 @@ public class Merchant {
     @Column(name = "is_active", nullable = false)
     private Boolean isActive;
 
+    @Column(name = "is_closed", nullable = false)
+    private Boolean isClosed;
+
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 }
